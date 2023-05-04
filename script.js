12d3b3ec97dc4528a28294f5e5748a555f93f470
Bruno Galvão <brunogalvao147@gmail.com>
2023-05-04 12:14:37 -0300

    historia geek

---
+++ script.js
@@ -0,0 +1,14 @@
+function mudarpagina() {
+    window.location.href = 'primeira-guerra.html';
+}
+
+function mudarpagina2() {
+    window.location.href = 'segunda-guerra.html';
+}
+
+function mudarpagina3() {
+    window.location.href = 'guerra-fria.html';
+}
+function btnvoltar() {
+    window.location.href = 'index.html';
+}
